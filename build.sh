c3f066c572786e32bea49d952c1968f91923f67b
Your Name <you@example.com>
2024-04-15 10:50:06 +0200

    y not

--- build.sh
+++ build.sh
@@ -1,19 +1,16 @@
 #!/bin/sh -ex
 
 cd `dirname $0`
-mkdir -p build
+rm -rf build
+mkdir -p build/fs/bin
 ./compilerpy/main.py build/mbr.bin boot/mbr.kc
 [ `stat -c %s build/mbr.bin` -gt 440 ] && echo "MBR too large" && exit 1
 ./compilerpy/main.py build/vbr.bin boot/vbr.kc
 [ `stat -c %s build/vbr.bin` -gt 448 ] && echo "VBR too large" && exit 1
 
-./compilerpy/main.py build/command.exe command/main.kc
-
-./compilerpy/main.py build/echo.exe bin/echo.kc
-./compilerpy/main.py build/dir.exe bin/dir.kc
-./compilerpy/main.py build/edit.exe bin/edit.kc
-./compilerpy/main.py build/touch.exe bin/touch.kc
-./compilerpy/main.py build/del.exe bin/del.kc
+echo command.kc bin/*.kc | tr ' ' $'\n' | xargs -I{} -P `nproc` sh -c './compilerpy/main.py build/fs/`echo {} | sed "s/kc/exe/"` "{}"'
+find command.kc bin lib boot -type d -exec mkdir -p build/fs/SRC/{} \;
+find command.kc bin lib boot -type f -exec sh -c 'fold -w 79 -s "{}" > "build/fs/SRC/{}"' \; -exec unix2dos build/fs/SRC/{} \;
 
 dd if=/dev/zero of=build/disk.img bs=1M count=32
 echo 'start=1, type=6, bootable' | sfdisk build/disk.img
@@ -21,16 +18,7 @@ dd if=build/mbr.bin of=build/disk.img conv=notrunc
 LOOP=`sudo losetup -f -P build/disk.img --show`
 sudo mkfs.vfat -F 16 "$LOOP"p1
 sudo mount "$LOOP"p1 /mnt
-sudo cp build/command.exe /mnt/COMMAND.EXE
-sudo mkdir /mnt/BIN
-sudo mkdir /mnt/SRC
-sudo find command bin lib boot -type d -exec mkdir -p /mnt/SRC/{} \;
-sudo find command bin lib boot -type f -exec sh -c 'fold -w 79 -s "{}" > "/mnt/SRC/{}"' \; -exec unix2dos /mnt/SRC/{} \;
-sudo cp build/echo.exe /mnt/BIN/ECHO.EXE
-sudo cp build/dir.exe /mnt/BIN/DIR.EXE
-sudo cp build/edit.exe /mnt/BIN/EDIT.EXE
-sudo cp build/touch.exe /mnt/BIN/TOUCH.EXE
-sudo cp build/del.exe /mnt/BIN/DEL.EXE
+sudo cp -r build/fs/* /mnt/
 sudo umount /mnt
 sudo losetup -d "$LOOP"
 dd if=build/vbr.bin of=build/disk.img bs=1 seek=574 conv=notrunc
